163429499f1d088cf7bc47b9401d6dc923261345
1lyasm <il.mystafa@gmail.com>
2023-10-17 19:00:56 +0300

    add index.tmpl

--- demo/counter/src/main.go
+++ demo/counter/src/main.go
@@ -1,21 +1,57 @@
 package main
 
 import (
+	"bytes"
+	"html/template"
+	"log"
 	"net/http"
-	"strconv"
+	"os"
+    "strconv"
 )
 
-type CounterHandler struct {
-	count uint64
+type TemplateData struct {
+    Count string
 }
 
-func (handler *CounterHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	handler.count += 1
-	w.Write([]byte(strconv.FormatUint(handler.count, 10)))
+type Server struct {
+    count uint64
 }
+
+func (server *Server) generateData() TemplateData {
+    return TemplateData{Count: strconv.FormatUint(server.count, 10)}
+}
+
+func (server *Server) fillHTML() {
+    template, err := template.New("index.tmpl").ParseFiles("index.tmpl")
+    if err != nil {
+        log.Fatalf("fillHTML: ParseFiles failed: %e", err)
+    }
+    template_data := server.generateData()
+    var buffer bytes.Buffer
+    err = template.Execute(&buffer, template_data)
+    if err != nil {
+        log.Fatalf("fillHtml: Execute failed: %e", err)
+    }
+    err = os.WriteFile("index.html", buffer.Bytes(), 0644)
+    if err != nil {
+        log.Fatalf("fillHTML: WriteFile failed: %e", err)
+    }
+}
+
+func (server *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+    path := req.URL.Path
+    if path == "/increment" {
+        server.count += 1
+    }
+    server.fillHTML();
+    http.ServeFile(w, req, "index.html")
+}
+
 
 func main() {
-    http.Handle("/", &CounterHandler{count: 0})
-    http.ListenAndServe(":6789", nil)
+    handler := &Server{count: 0}
+	http.Handle("/", handler)
+	http.Handle("/increment", handler)
+	http.ListenAndServe(":6789", nil)
 }
 
